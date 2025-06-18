3f17c74d7f8ff4f33b91a483d27e567141378ded
Srini Karlekar <skarlekar@yahoo.com>
2025-06-18 11:44:49 -0400

    added Heroku dependencies

---
+++ heroku-env-setup.sh
@@ -0,0 +1,5 @@
+heroku git:remote -a doc2graph
+heroku config:set NEO4J_URL="neo4j+s://1265a97b.databases.neo4j.io"
+heroku config:set NEO4J_USERNAME="neo4j"
+heroku config:set NEO4J_PASSWORD="OMvVeHWbQgzuDkXqFxPTFp115SSLrg_Sb7R_zbBqzS4"
+heroku config:set OPENAI_API_KEY="k-proj-tgRUnQI5JZd05k1r0wruejt-dyM3EI6spz48u-kIT2l9qhpkOGwa413FC1tBY7vams2TsnTNzYT3BlbkFJ4J7a69Ibl0IQJmyZsuhonBVe8Hkm16Mm0Pp7Go7MVjs867656RP11sTjy06oIfx_eQBfHGrWwA"
